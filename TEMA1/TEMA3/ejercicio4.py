f681c533edb70cdea25ac845d648c8e229bf5a5b
elmer-110 <ecedillov@student.universidadunie.com>
2025-06-18 01:16:19 +0200

    conjuntos

---
+++ TEMA1/TEMA3/ejercicio4.py
@@ -0,0 +1,7 @@
+Madrid=["alex","jordan","mia","Leo", "Sophia","antonio"]
+Atletic=["antonio", "Emma", "Mia", "Oliver", "Alex", "Liam"]
+
+
+###### Las listas las creamos a Conjuntos ######
+conj_madrid=set(Madrid)
+conj_Atletic=set(Atletic)
