c857b58a7a1c4dd254d592a7f5b60c9543de1e93
elmer-110 <ecedillov@student.universidadunie.com>
2025-06-26 01:13:23 +0200

    no se

--- examen global/ejercicio_registro.py
+++ examen global/ejercicio_registro.py
@@ -2,9 +2,13 @@ def registrar_participantes():
     nombres=[]
     while True:
         solicitar_nombre=input("introduzca su nombre:")
-        if solicitar_nombre==("FIN"):
+        if solicitar_nombre=="FIN":
             break
         nombres.append(solicitar_nombre)
-
-    return 
+    return(nombres)
 print(registrar_participantes())
+
+participantes_elegidos=[]
+def identificar_posiciones(participantes,participantes_elegidos):
+    nombres
+
