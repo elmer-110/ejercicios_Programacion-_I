abd002daf5afe217803c6d35f734e18adfc8d935
elmer-110 <ecedillov@student.universidadunie.com>
2025-06-26 12:41:33 +0200

    rayado

--- examen global/ejercicio_registro.py
+++ examen global/ejercicio_registro.py
@@ -6,9 +6,34 @@ def registrar_participantes():
             break
         nombres.append(solicitar_nombre)
     return(nombres)
-print(registrar_participantes())
+
 
 participantes_elegidos=[]
 def identificar_posiciones(participantes,participantes_elegidos):
-    nombres
+    posiciones={}
+    for elegido in participantes_elegidos:
+        if elegido in participantes:
+            posiciones[elegido]=participantes.index(elegido)### .index
+        else:
+            posiciones[elegido]=None
+    return posiciones
+
+
+def actualizar_registro(participantes,poscion,nuevos_participantes):
+    posicion=int(input("introduce la posicion del participante a reemplazar:"))
+    if 0<= posicion < len(participantes):
+        nuevos_participantes=input("introduzca el nuevo nombre:")
+    else:
+        print("posicion no valida ")
+    return 
+
+
+print(registrar_participantes())
+participantes=registrar_participantes()
+print(identificar_posiciones(participantes,participantes_elegidos))
+print(actualizar_registro(participantes,poscion,nuevos_participantes))
+
+participantes_elegidos=["manuel","jose","miguel"]
+participantes[]=
+
 
