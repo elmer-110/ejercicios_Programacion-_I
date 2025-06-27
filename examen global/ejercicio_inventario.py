839adfedd4b1f41ccf10698fade198e5e60b03ce
elmer-110 <ecedillov@student.universidadunie.com>
2025-06-27 09:56:24 +0200

    yaeta

--- examen global/ejercicio_inventario.py
+++ examen global/ejercicio_inventario.py
@@ -16,3 +16,21 @@ def actualizacion_de_precios(inventario):
     for libro in inventario:
         precio_compra = libro.get("precio", 0)
         libro["precio_venta"] = round(precio_compra * 1.30, 2)
+    return inventario
+
+
+
+
+def generar_reporte_inventario(inventario):
+    print("\n--- REPORTE DE INVENTARIO ---")
+    for libro in inventario:
+        print(f"Título: {libro['título']}")
+        print(f"Autor: {libro['autor']}")
+        print(f"Precio Compra: {libro['precio']:.2f} €")
+        print(f"Precio Venta: {libro.get('precio_venta', 0):.2f} €")
+        print("-" * 30)
+inventario_libros=registrar_inventario()
+actualizar_precios=(inventario_libros())
+generar_reporte_inventario(inventario_libros)
+        
+        
